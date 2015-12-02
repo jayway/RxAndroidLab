58feb0872293f330f539cab7e9982089f9bf5f30
Albin Theander <Albin.Theander@jayway.com>
2015-12-02 09:39:43 +0100

    Made MainActivity a bit more reactive

--- app/src/main/java/com/jayway/klab/rxandroidlab/MainActivity.java
+++ app/src/main/java/com/jayway/klab/rxandroidlab/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.jakewharton.rxbinding.view.RxView;
 import com.jayway.klab.rxandroidlab.albumsearch.AlbumSearchActivity;
 import com.jayway.klab.rxandroidlab.signup.SignUpActivity;
 
@@ -17,16 +18,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-    }
 
-    @OnClick(R.id.register_button)
-    public void launchRegisterActivity(View view) {
-        startActivity(new Intent(this, SignUpActivity.class));
-    }
+        RxView.clicks(findViewById(R.id.searchlab_button))
+                .subscribe(aVoid -> startActivity(new Intent(this, AlbumSearchActivity.class)));
 
-    @OnClick(R.id.searchlab_button)
-    public void launchAlbumSearchActivity(View view) {
-        startActivity(new Intent(this, AlbumSearchActivity.class));
+        RxView.clicks(findViewById(R.id.register_button))
+                .subscribe(aVoid -> startActivity(new Intent(this, SignUpActivity.class)));
     }
 }
